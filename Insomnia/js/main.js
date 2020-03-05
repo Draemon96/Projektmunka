978e8815c20bf08ad1a705e3e0b957e7df8ad4df
Draemon96 <davicku@hotmail.com>
2020-03-05 16:50:19 +0100

    közösencsináltuknyeheehehehe

--- Insomnia/js/main.js
+++ Insomnia/js/main.js
@@ -3,9 +3,7 @@ import Timer from './timer.js';
 import Entity from './Entity.js';
 import {loadLevel} from './loader.js';
 import {createMario} from './entities.js';
-import {createCollisionLayer, createCameraLayer} from './layers.js';
 import {setupKeyboard} from './input.js';
-import {setupMouseControl} from './debug.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
@@ -30,11 +28,13 @@ Promise.all([
 	const input = setupKeyboard(mario);
 	input.listenTo(window);
 
-	setupMouseControl(canvas, mario, camera);
-
 	const timer = new Timer(1/60);
 	timer.update=function update(deltaTime){
 		level.update(deltaTime);
+
+		if(mario.pos.x > 100){
+			camera.pos.x = mario.pos.x -100;
+		}
 
 		level.comp.draw(context, camera);
 	}
