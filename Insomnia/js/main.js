56de0a5699dac9c077c8b8726cc099fff5ec27ca
Draemon96 <55750367+Draemon96@users.noreply.github.com>
2020-03-07 02:17:16 +0100

    episode 10 is done :P már csak 6 rész van hátra :(

--- Insomnia/js/main.js
+++ Insomnia/js/main.js
@@ -2,28 +2,42 @@ import Camera from './Camera.js';
 import Timer from './timer.js';
 import Entity from './Entity.js';
 import {loadLevel} from './loaders/level.js';
-import {createMario} from './entities.js';
+import {loadEntities} from './entities.js';
 import {setupKeyboard} from './input.js';
+import {createCollisionLayer} from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
 Promise.all([
-	createMario(),
+	loadEntities(),
 	loadLevel('1-1'),
 ])
-.then(([mario, level]) => {
+.then(([entitiy, level]) => {
+	console.log(entitiy);
+
 	const camera = new Camera();
 
 	window.camera = camera;
 
+	const mario = entitiy.mario();
 	mario.pos.set(64, 64);
 
 	/*level.comp.layers.push(
 		createCollisionLayer(level),
 		createCameraLayer(camera));*/
 
+	const goomba = entitiy.goomba();
+	goomba.pos.x = 220;
+	level.entities.add(goomba);
+
+	const koopa = entitiy.koopa();
+	koopa.pos.x = 260;
+	level.entities.add(koopa);
+
 	level.entities.add(mario);
+
+	//level.comp.layers.push(createCollisionLayer(level));
 
 	const input = setupKeyboard(mario);
 	input.listenTo(window);
